f42543935b4efc60dca5c824e599e02813f89569
Sanya <purvi.sanya@gmail.com>
2020-10-29 09:03:59 +0400

    add this

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+// ;alskdjf;skdfjsa;lkfjs;adlkj;askfjs;
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
